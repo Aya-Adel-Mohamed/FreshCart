320fbbe600f4eda51c03740cd5c19106e922d7ec
Aya-Adel-Mohamed <aya90318@gmail.com>
2024-02-01 11:18:46 -0800

    notFound component

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import notFound from '../../assets/error.svg';
+import styles from './NotFound.module.css'
 
 const NotFound = () => {
     return (  
         <>
+  <section className="d-flex justify-content-center align-items-center">
+  <img src={notFound} alt="" className={styles.image}/>
+  </section>
         </>
     );
 }
