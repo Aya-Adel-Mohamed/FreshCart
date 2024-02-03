8d62f345b9c023c9cb050c1b79e8138806c155f4
Aya-Adel-Mohamed <aya90318@gmail.com>
2024-02-03 11:47:31 -0800

    FeaturedProductsResponsive

--- src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './FeaturedProducts.module.css';
 import axios from 'axios';
 import Loading from '../Loading/Loading';
+import { Link } from 'react-router-dom';
 const FeaturedProducts = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -20,8 +21,9 @@ const FeaturedProducts = () => {
         <>
                 <div className="row mt-5">
             {products?.map((product,index)=>
-            <div key={index} className="col-md-2 ">
-                <div className="product px-3 py-3 cursor-pointer">
+            <div key={index} className="col-xl-2 col-lg-3 col-md-4">
+         <Link to={`/productdetails/${product.id}`} className='productDetails'>
+         <div className="product px-3 py-3 cursor-pointer">
                     <img src={product.imageCover} className='w-100 mb-2' alt="" />
                     <span className='font text-main font-sm pt-5'>{product.category.name}</span>
                     <h3 className='fw-bolder h6'>{product.title.split(' ').slice(0,2).join(' ')}</h3>
@@ -31,6 +33,7 @@ const FeaturedProducts = () => {
                     </div>
                     <button className='btn bg-main text-white w-100 mt-2'><i className="fa-solid fa-cart-shopping me-2"></i> Add to cart</button>
                 </div>
+         </Link>
             </div>
             )}
         </div>
