6c4905fe1d72b2f8749d555ae664efc85ac1e757
Aya-Adel-Mohamed <aya90318@gmail.com>
2024-02-13 12:08:32 -0800

    react-detect-offline

--- src/Components/MainLayout/MainLayout.jsx
+++ src/Components/MainLayout/MainLayout.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-import { Outlet } from 'react-router-dom/dist/umd/react-router-dom.development';
-
+import { Outlet } from 'react-router-dom';
+import { Offline } from 'react-detect-offline';
 
 const MainLayout = ({userData,logOut}) => {
     return (
@@ -10,6 +10,13 @@ const MainLayout = ({userData,logOut}) => {
         <Navbar userData={userData} logOut={logOut}/>
         <div className="container px-5 px-md-4 px-lg-3 px-xl-0">
             <Outlet></Outlet>
+        </div>
+        <div>
+            <Offline>
+                <div className='network'>
+                    <i className='fas fa-wifi me-2 text-main'></i> you're offline
+                </div>
+            </Offline>
         </div>
         <Footer/>
         </>
